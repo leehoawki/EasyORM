47bf07c15d5c3c11baa46bc176726cdc47f9db55
leehoawki <leehoawki@gmail.com>
2015-12-23 12:17:37 +0800

    ORM Testcase added

--- ORM.py
+++ ORM.py
@@ -1,24 +1,48 @@
 class Field(object):
-    pass
+    def __init__(self, name, pk=False):
+        self.name = name
+        self.pk = pk
+
+    def __str__(self):
+        s = '<%s:%s:' % (self.__class__.__name__, self.name)
+        if self.pk:
+            s += 'Primary Key'
+        s += '>'
+        return s
 
 
 class ORM(type):
     def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return super(ORM, cls).__new__(cls, name, bases, attrs)
+
         mappings = {}
         fields = []
-        params = []
+        pk = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                mappings[k] = v
-
+                if v.pk:
+                    if pk:
+                        raise Exception('You can not define more than 1 primary key in Class: %s' % name)
+                    else:
+                        pk = v
+                mappings[v.name] = v
+                attrs.pop(k)
+        if not pk:
+            raise Exception('You need to define 1 primary key in Class: %s' % name)
         for k, v in mappings.items():
             fields.append(k)
-            params.append('?')
-            attrs.pop(k)
 
         attrs['__mappings__'] = mappings
         attrs['__table__'] = name
-        attrs['__insert_sql__'] = 'insert into %s (%s) values (%s)' % (name, ','.join(fields), ','.join(params))
+        attrs['__fields__'] = fields
+        attrs['__pk__'] = pk
+        attrs['__insert_sql__'] = 'insert into %s (%s) values (%s)' % (
+            name, ','.join(fields), ','.join(['?'] * len(fields)))
+        attrs['__update_sql__'] = 'update %s set %s  where %s = %s' % (
+            name, ' and '.join('%s = ?' % x for x in fields if x != pk.name), pk.name, '?')
+        attrs['__delete_sql__'] = 'delete from %s where %s = %s' % (name, pk.name, '?')
+        attrs['__select_one_sql__'] = 'select %s from %s where %s = %s' % (",".join(fields), name, pk.name, '?')
         return super(ORM, cls).__new__(cls, name, bases, attrs)
 
 
@@ -42,14 +66,19 @@ class Model(dict):
         for k, v in self.__mappings__.items():
             args.append(self.__getattr__(k))
         print self.__insert_sql__
-        print args
 
     def update(self):
-        pass
+        print self.__update_sql__
 
     def delete(self):
-        pass
+        print self.__delete_sql__
 
     @classmethod
     def select(cls, **kwargs):
-        pass
+        sql = 'select %s from %s where %s' % (
+            ",".join(cls.__fields__), cls.__table__, ' and '.join('%s = ?' % x for x in kwargs))
+        print sql
+
+    @classmethod
+    def select_one(cls, val):
+        print cls.__select_one_sql__
--- ORM_Test.py
+++ ORM_Test.py
@@ -1,23 +1,30 @@
 from ORM import *
+import logging
 
 
 class Person(Model):
-    id = Field()
-    name = Field()
-    email = Field()
-    password = Field()
+    id = Field("id", pk=True)
+    name = Field("name")
+    email = Field("email")
+    password = Field("password")
 
+
+logging.basicConfig(level=logging.DEBUG)
 
 print Person.select(id=12345)
-
+print Person.select_one(12345)
 u = Person(id=12345, name='Michael', email='test@orm.org', password='my-pwd')
 u.insert()
 print Person.select(id=12345)
+print Person.select_one(12345)
 
-u.password = "wahaha"
+u.password = "WhatTheFuck"
 u.update()
-print Person.select(id=12345)
+print Person.select(password="WhatTheFuck")
+print Person.select(password="my-pwd")
+print Person.select_one(12345)
 
 Person.delete(u)
 print Person.select(id=12345)
+print Person.select_one(12345)
 print dir(Person)
